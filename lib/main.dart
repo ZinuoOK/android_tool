afcfd1277319811991cf5e86fc8f5c1d305e244f
wll272243 <wll272243@alibaba-inc.com>
2022-09-30 22:20:20 +0800

    更新主题配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:android_tool/page/main/main_page.dart';
 import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +16,20 @@ class MyApp extends StatelessWidget {
       title: 'AndroidADBTool',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
+      // theme: _buildTheme(Brightness.dark),
       home: const MainPage(),
     );
   }
+
+  // theme config https://pub.flutter-io.cn/packages/google_fonts
+
+  // ThemeData _buildTheme(brightness) {
+  //   var baseTheme = ThemeData(brightness: brightness);
+  //
+  //   return baseTheme.copyWith(
+  //     textTheme: GoogleFonts.latoTextTheme(baseTheme.textTheme),
+  //   );
+  // }
 }
